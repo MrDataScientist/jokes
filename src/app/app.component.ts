fc41c7c8406d4bf3d06e0dc00300888c71909e93
Tarik En Nakdi <tarik.ennakdi@kaiser-x.com>
2018-10-18 15:04:46 +0200

    PWA demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'jokes';
+  title = 'app';
+
+  update = false;
+  joke: any;
+
+  constructor(updates: SwUpdate, private data: DataService) {
+    updates.available.subscribe(event => {
+
+      //this.update = true;
+      updates.activateUpdate().then(() => document.location.reload());
+
+    });
+  }
+
+  ngOnInit() {
+    this.data.gimmeJokes().subscribe(res => {
+      this.joke = res;
+      console.log(res);
+    });
+  }
+
+
+
+
+
 }
